ce40a071f0a085ff2e1f4e2c9ea165c540c2aa74
Anderson Costa <anderson.costa@meliuz.com.br>
2022-01-12 15:35:09 -0300

    feat: Adiciona tipagem para variavel

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { IPlanet } from "../../types/types";
 
-import {Container, CardView, ImageCard, Title } from './styles';
+import {
+  Container, 
+  CardView, 
+  ImageCard, 
+  Title, 
+  InfoView, 
+  InfoPlanet, 
+  TextView
+ } from './styles';
 
-const Card : React.FC = () => {
+ 
+
+const Card : React.FC<IPlanet> = ({
+  name,
+  image,
+  temperature,
+  size
+}) => {
   return(
     <Container>
      <CardView>
-      <ImageCard/>
-      <Title>Texto</Title>
+      <ImageCard uri={image}/>
+      <TextView>
+       <Title>{name}</Title>
+        <InfoView>
+          <InfoPlanet>{temperature}</InfoPlanet>
+          <InfoPlanet>{size}</InfoPlanet>
+        </InfoView>
+      </TextView>
       </CardView>
     </Container>
   );

8d7194bc7b25aec751267fe5e978311dfa099d87
Anderson Costa <anderson.costa@meliuz.com.br>
2022-01-12 11:07:05 -0300

    feat: Adiciona index do componente Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "../../components/Card";
+
+import {Container, Image} from "./styles"
+
+ const Home: React.FC = () => {
+  return(
+    <Image>
+      <Container>
+        <Card/>
+       </Container>
+    </Image>
+  );
+};
+
+export default Home;

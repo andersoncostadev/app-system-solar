337cacbb9e33aa5d414f217fd507a47d760235e8
Anderson Costa <anderson.costa@meliuz.com.br>
2022-01-17 18:39:55 -0300

    chore: Adiciona load para pagina planets

--- src/screens/Planets/index.tsx
+++ src/screens/Planets/index.tsx
@@ -4,11 +4,13 @@ import Card from '../../components/Card';
 import api from '../../services/api';
 import {IPlanet} from '../../types/types';
 import {useNavigation} from '@react-navigation/native';
+import LottieView from 'lottie-react-native';
 
-import {Container, Image, TouchablePlanet} from './styles';
+import {Container, Image, Load, TouchablePlanet} from './styles';
 
 const Planets: React.FC = () => {
   const [data, setData] = useState<IPlanet[]>([]);
+  const [isload, setIsLoad] = useState(false);
 
   const nav = useNavigation();
 
@@ -17,13 +19,32 @@ const Planets: React.FC = () => {
   };
 
   useEffect(() => {
+    setIsLoad(true);
     api
       .get('/planets')
       .then(response => {
         setData(response.data);
       })
-      .catch(() => alert('Houve um erro ao consultar a api'));
+      .catch(() => alert('Houve um erro ao consultar a api'))
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoad(false);
+        }, 2000);
+      });
   }, []);
+
+  if (isload) {
+    return (
+      <Load>
+        <LottieView
+          source={require('../../animation/17059-galacticons-animated.json')}
+          autoPlay
+          loop
+          style={{height: 500, width: 500}}
+        />
+      </Load>
+    );
+  }
 
   return (
     <Image>
